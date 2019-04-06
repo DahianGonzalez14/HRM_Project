0fbb4dd372436f54a2b8360ad5c8d7dbff12b044
Dahian Antonio González Camacho <41456760+DahianGonzalez14@users.noreply.github.com>
2019-04-05 22:58:13 -0400

    Modulo de procesos agregado

---
+++ ProyectoFinalASP/Controllers/NominasController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinalASP.Models;
+
+namespace ProyectoFinalASP.Controllers
+{
+    public class NominasController : Controller
+    {
+        private ProyectoFinalAspEntities db = new ProyectoFinalAspEntities();
+
+        // GET: Nominas
+        public ActionResult Index()
+        {
+            var nominas = db.Nominas.Include(n => n.Empleados);
+            return View(nominas.ToList());
+        }
+
+        // GET: Nominas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nominas nominas = db.Nominas.Find(id);
+            if (nominas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nominas);
+        }
+
+        // GET: Nominas/Create
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+        // POST: Nominas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,MontoTotal,Mes,Año")] Nominas nominas)
+        {
+            Nullable<Decimal> total = (from result in db.Empleados where result.Estatus=="Activo" select result.Salario).Sum();
+            if (ModelState.IsValid)
+            {
+                db.Nominas.Add(nominas);
+                nominas.MontoTotal = total;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FK_Empleado = new SelectList(db.Empleados, "ID", "Nombre", nominas.FK_Empleado);
+            return View(nominas);
+        }
+
+        // GET: Nominas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nominas nominas = db.Nominas.Find(id);
+            if (nominas == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FK_Empleado = new SelectList(db.Empleados, "ID", "Nombre", nominas.FK_Empleado);
+            return View(nominas);
+        }
+
+        // POST: Nominas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,MontoTotal,FK_Empleado,Mes,Año")] Nominas nominas)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                db.Entry(nominas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FK_Empleado = new SelectList(db.Empleados, "ID", "Nombre", nominas.FK_Empleado);
+            return View(nominas);
+        }
+
+        // GET: Nominas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nominas nominas = db.Nominas.Find(id);
+            if (nominas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nominas);
+        }
+
+        // POST: Nominas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Nominas nominas = db.Nominas.Find(id);
+            db.Nominas.Remove(nominas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoFinalASP/Controllers/PermisosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinalASP.Models;
+
+namespace ProyectoFinalASP.Controllers
+{
+    public class PermisosController : Controller
+    {
+        private ProyectoFinalAspEntities db = new ProyectoFinalAspEntities();
+
+        // GET: Permisos
+        public ActionResult Index()
+        {
+            var permisos = db.Permisos.Include(p => p.Empleados);
+            return View(permisos.ToList());
+        }
+
+        // GET: Permisos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permisos permisos = db.Permisos.Find(id);
+            if (permisos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permisos);
+        }
+
+        // GET: Permisos/Create
+        public ActionResult Create()
+        {
+            ViewBag.FK_Empleado = new SelectList(db.Empleados, "ID", "Nombre");
+            return View();
+        }
+
+        // POST: Permisos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,FK_Empleado,Desde,Hasta,Comentarios")] Permisos permisos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Permisos.Add(permisos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FK_Empleado = new SelectList(db.Empleados, "ID", "Nombre", permisos.FK_Empleado);
+            return View(permisos);
+        }
+
+        // GET: Permisos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permisos permisos = db.Permisos.Find(id);
+            if (permisos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FK_Empleado = new SelectList(db.Empleados, "ID", "Nombre", permisos.FK_Empleado);
+            return View(permisos);
+        }
+
+        // POST: Permisos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FK_Empleado,Desde,Hasta,Comentarios")] Permisos permisos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(permisos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FK_Empleado = new SelectList(db.Empleados, "ID", "Nombre", permisos.FK_Empleado);
+            return View(permisos);
+        }
+
+        // GET: Permisos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permisos permisos = db.Permisos.Find(id);
+            if (permisos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permisos);
+        }
+
+        // POST: Permisos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Permisos permisos = db.Permisos.Find(id);
+            db.Permisos.Remove(permisos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
